aa68ef5bba92df33cb1418a7b4f7363dded7e34b
noname33-cmd <marlonka20000@gmail.com>
2025-08-14 16:37:48 +0500

    Delete jboss_controller directory

--- jboss_controller/scripts/deploy_agents_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-NODES="node_97 node_148 node_150 node_151 node_152 node_120 node_121"
-for n in $NODES; do
-  ssh $n "/minjust-74-250/gbdfl/pattern_controller/scripts/run_agent_node.sh $n"
-done
