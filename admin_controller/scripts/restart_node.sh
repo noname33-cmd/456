4ac00ce067aa6bdad96a0838a54e4cf6bcd44e0c
noname33-cmd <marlonka20000@gmail.com>
2025-08-15 14:12:01 +0500

    Delete admin_controller directory

--- admin_controller/scripts/restart_node.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-NODE="$1"
-if [ -z "$NODE" ]; then
-    echo "Usage: $0 node_name"
-    exit 1
-fi
-
-echo "request_reboot node=${NODE}" > /minjust-74-250/gbdfl/pattern_controller/signals/restart_${NODE}.txt
